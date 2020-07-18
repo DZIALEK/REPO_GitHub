090a88673bcc861fbd01d002a1ea69fba94078a5
Paweł Dziatłowicz <pawel.dziatlowicz@microsoft.wsei.edu.pl>
2020-07-18 09:05:03 +0200

    dsadsa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Lekcja0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            if (a > 0 && a >=5)
+            {
+                Console.WriteLine("a większe od zera | " + a);
+                Console.WriteLine("a większe od 5 | " + a);
+            }
+            else if (a > 0)
+            {
+                Console.WriteLine("a większe od zera | " + a);
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine("Pokazuje sie a, bo jest równe zero | " + a);
+            }
+            else
+            {
+                Console.WriteLine("Pokazuje sie a, bo if mniejsze od zera | " + a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/cwicz2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Zmienne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short s;
+            int i;
+            double d;
+            // Inicjowanie zmiennych
+            s = 10;
+            i = 20;
+            d = s + i;
+            // Wyświetlenie w konsoli wartości kolejnych zmiennych
+            // Rezultat wykonania programu: s = 10, i = 20, d = 30
+            Console.WriteLine("s = {0}, i = {1}, d = {2}", s, i, d);
+            Console.ReadLine();
+        }
+    }
+}
